e0315cf11f90a88f6a96070f9036900d7849b315
luyh <yanhonglu5@icloud.com>
2019-09-29 14:57:42 +0800

    随机S,G,修改R

--- contents/1_command_line_reinforcement_learning/treasure_on_right.py
+++ contents/1_command_line_reinforcement_learning/treasure_on_right.py
@@ -10,16 +10,16 @@ import numpy as np
 import pandas as pd
 import time
 
-np.random.seed(2)  # reproducible
+#np.random.seed(2)  # reproducible
 
 
-N_STATES = 6   # the length of the 1 dimensional world
+N_STATES = 15   # the length of the 1 dimensional world
 ACTIONS = ['left', 'right']     # available actions
 EPSILON = 0.9   # greedy police
 ALPHA = 0.1     # learning rate
 GAMMA = 0.9    # discount factor
-MAX_EPISODES = 13   # maximum episodes
-FRESH_TIME = 0.3    # fresh time for one move
+MAX_EPISODES = 20   # maximum episodes
+FRESH_TIME = 0.2    # fresh time for one move
 
 
 def build_q_table(n_states, actions):
@@ -27,7 +27,7 @@ def build_q_table(n_states, actions):
         np.zeros((n_states, len(actions))),     # q_table initial values
         columns=actions,    # actions's name
     )
-    # print(table)    # show table
+    #print(table)    # show table
     return table
 
 
@@ -41,27 +41,36 @@ def choose_action(state, q_table):
     return action_name
 
 
-def get_env_feedback(S, A):
+def get_env_feedback(S,G, A):
     # This is how agent will interact with the environment
     if A == 'right':    # move right
-        if S == N_STATES - 2:   # terminate
+        if S == G - 1:   # terminate
             S_ = 'terminal'
-            R = 1
+            R = 100
+        elif S == N_STATES - 1:
+            S_ = S  # reach the wall
+            R = -10
         else:
             S_ = S + 1
-            R = 0
+            R = -1
+
+
     else:   # move left
-        R = 0
-        if S == 0:
+        if S == G + 1:  # terminate
+            S_ = 'terminal'
+            R = 100
+        elif S == 0:
             S_ = S  # reach the wall
+            R = -10
         else:
             S_ = S - 1
+            R = -1
     return S_, R
 
-
-def update_env(S, episode, step_counter):
+def update_env(S, G,episode, step_counter):
     # This is how environment be updated
-    env_list = ['-']*(N_STATES-1) + ['T']   # '---------T' our environment
+    env_list = ['-']*(N_STATES)     # '---------T' our environment
+    env_list[G] = 'T'
     if S == 'terminal':
         interaction = 'Episode %s: total_steps = %s' % (episode+1, step_counter)
         print('\r{}'.format(interaction), end='')
@@ -77,15 +86,19 @@ def update_env(S, episode, step_counter):
 def rl():
     # main part of RL loop
     q_table = build_q_table(N_STATES, ACTIONS)
+    G = np.random.choice(N_STATES)
     for episode in range(MAX_EPISODES):
         step_counter = 0
-        S = 0
+
+        S = np.random.choice(N_STATES)
+        while S == G:
+            S = np.random.choice(N_STATES)
         is_terminated = False
-        update_env(S, episode, step_counter)
+        update_env(S, G,episode, step_counter)
         while not is_terminated:
 
             A = choose_action(S, q_table)
-            S_, R = get_env_feedback(S, A)  # take action & get next state and reward
+            S_, R = get_env_feedback(S,G, A)  # take action & get next state and reward
             q_predict = q_table.loc[S, A]
             if S_ != 'terminal':
                 q_target = R + GAMMA * q_table.iloc[S_, :].max()   # next state is not terminal
@@ -96,8 +109,10 @@ def rl():
             q_table.loc[S, A] += ALPHA * (q_target - q_predict)  # update
             S = S_  # move to next state
 
-            update_env(S, episode, step_counter+1)
+            update_env(S, G,episode, step_counter+1)
             step_counter += 1
+
+        #print(q_table)
     return q_table
 
 
